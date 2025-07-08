63423737ff12ed8d840ba7f159fdb068fae75784
Helen <25727036+helenzhou6@users.noreply.github.com>
2025-07-08 16:06:20 +0000

    Update bes example to train for multiple Epochs

--- src/bes-example.py
+++ src/bes-example.py
@@ -3,61 +3,59 @@ import whisper
 from utils import get_device 
 
 FILE_PATH = "src/Helen--Bes.m4a"
+EPOCHS = 3
+LEARNING_RATE = 1e-5
 
-device = get_device()
-model = whisper.load_model("tiny", device=device)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
-criterion = torch.nn.CrossEntropyLoss()
-tokenizer = whisper.tokenizer.get_tokenizer(multilingual=True)
+def train(model, audio, tokenizer):
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    criterion = torch.nn.CrossEntropyLoss()
 
-audio = whisper.load_audio(FILE_PATH)
-audio = whisper.pad_or_trim(audio)
-log_mel = whisper.log_mel_spectrogram(audio).to(device)
+    ids = []
+    ids += [tokenizer.sot]
+    ids += [tokenizer.language_token]
+    ids += [tokenizer.transcribe]
+    ids += tokenizer.encode(" Hello, my name is Bes.")
+    ids += [tokenizer.eot]
+    train_tokens = torch.tensor(ids, device=device).unsqueeze(0)
 
-def whisper_without_fine_tuning(model, file_path, device):
+    mel_unsqueezed = whisper.log_mel_spectrogram(audio).unsqueeze(0).to(device)
+
+    for epoch in range(EPOCHS):
+        print(f"----Epoch {epoch + 1}/{EPOCHS}----")
+
+        prediction = model(tokens=train_tokens, mel=mel_unsqueezed)
+        target = train_tokens[:, 1:].contiguous()  # Skip the start token
+
+        loss = criterion(prediction.transpose(1, 2), train_tokens)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        prediction = model(tokens=train_tokens, mel=mel_unsqueezed)
+        prediction = prediction[:, :-1, :].contiguous()  # Remove the last token
+
+        print("Loss: ", loss.item())
+        # print("Ids Target: ", target.squeeze().tolist())
+        # print("Ids Pred: ", torch.argmax(prediction, dim=-1).squeeze().tolist())
+        print("Text target: ", tokenizer.decode(target.squeeze().tolist()))
+        print("Text pred: ", tokenizer.decode(torch.argmax(prediction, dim=-1).squeeze().tolist()))
+
+def whisper_without_fine_tuning(model, audio, device):
     options = whisper.DecodingOptions()
+    log_mel = whisper.log_mel_spectrogram(audio).to(device)
     response = whisper.decode(model, log_mel, options)
     return response.text
 
-print("whisper prediction without fine tuning: ", whisper_without_fine_tuning(file_path=FILE_PATH, model=model, device=device))
+if __name__ == "__main__":
+    device = get_device()
+    model = whisper.load_model("tiny", device=device)
 
-# Preparing input for target for the model to train on and learn
-model.train()
+    tokenizer = whisper.tokenizer.get_tokenizer(multilingual=True)
 
-ids = []
-ids += [tokenizer.sot]
-ids += [tokenizer.language_token]
-ids += [tokenizer.transcribe]
-ids += tokenizer.encode(" Hello, my name is Bes.")
-ids += [tokenizer.eot]
-train_tokens = torch.tensor(ids, device=device).unsqueeze(0)
+    audio = whisper.load_audio(FILE_PATH)
+    audio = whisper.pad_or_trim(audio)
+    
+    print("Whisper prediction without fine tuning: ", whisper_without_fine_tuning(model, audio, device))
 
-mel_unsqueezed = whisper.log_mel_spectrogram(audio).unsqueeze(0).to(device)
-prediction = model(tokens=train_tokens, mel=mel_unsqueezed)
-target = train_tokens[:, 1:].contiguous()  # Skip the start token
-
-print("--- Before training ---")
-print("Ids Target: ", target.squeeze().tolist())
-print("Ids Pred: ", torch.argmax(prediction, dim=-1).squeeze().tolist())
-print("Text target: ", tokenizer.decode(target.squeeze().tolist()))
-print("Text pred: ", tokenizer.decode(torch.argmax(prediction, dim=-1).squeeze().tolist()))
-
-# Training the model
-loss = criterion(prediction.transpose(1, 2), train_tokens)
-print("Loss: ", loss.item())
-optimizer.zero_grad()
-loss.backward()
-optimizer.step()
-
-print("--- After training ---")
-model.eval()
-prediction = model(tokens=train_tokens, mel=mel_unsqueezed)
-prediction = prediction[:, :-1, :].contiguous()  # Remove the last token
-
-print("Ids Target: ", target.squeeze().tolist())
-print("Ids Pred: ", torch.argmax(prediction, dim=-1).squeeze().tolist())
-print("Text target: ", tokenizer.decode(target.squeeze().tolist()))
-print("Text pred: ", tokenizer.decode(torch.argmax(prediction, dim=-1).squeeze().tolist()))
-
-loss = criterion(prediction.transpose(1, 2), target)
-print("Loss: ", loss.item())
+    train(model, audio, tokenizer)
