c4e65118caac9cd9c30a9aed3713dc2b578dc6dd
Clement Ha <Clemha@gmail.com>
2025-07-10 13:33:07 +0100

    code to load the Mozilla dataset with 7K welsh sentences

---
+++ src/test_welsh_mozilla_dataset.py
@@ -0,0 +1,13 @@
+from datasets import load_dataset
+
+# Load Welsh subset, validated split (use 'train' or 'test' too)
+ds = load_dataset(
+    "mozilla-foundation/common_voice_13_0",
+    "cy",   # Welsh language code
+    split="train",
+      #use_auth_token=True  # 'train', 'validation', 'test'
+)
+
+print(ds)
+print(ds[0])
+
